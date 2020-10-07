8c4cea9e2705b7ced8b43ad5bf874be1c920b500
peergrammer <phdwight@gmail.com>
2020-10-07 20:03:01 +0800

    PopupMenu using Provider
    PopupMenu using provider

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -36,8 +36,24 @@ class ProductsProvider with ChangeNotifier {
           'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
     ),
   ];
+
+  var _showFavoritesOnly = false;
+
   List<Product> get items {
+    if (_showFavoritesOnly) {
+      return _items.where((element) => element.isFavorite).toList();
+    }
     return [..._items];
+  }
+
+  void showFavoritesOnly() {
+    _showFavoritesOnly = true;
+    notifyListeners();
+  }
+
+  void showAll() {
+    _showFavoritesOnly = false;
+    notifyListeners();
   }
 
   Product findById(String id) {
--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,12 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:myshop/providers/products.dart';
+import 'package:provider/provider.dart';
 import '../widgets/products_grid.dart';
+
+enum FilterOption {
+  Favorites,
+  All,
+}
 
 class ProductsOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final productsContainer =
+        Provider.of<ProductsProvider>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: Text('My Shop'),
+        actions: [
+          PopupMenuButton(
+            onSelected: (FilterOption value) {
+              if (value == FilterOption.Favorites) {
+                productsContainer.showFavoritesOnly();
+              } else {
+                productsContainer.showAll();
+              }
+            },
+            itemBuilder: (context) {
+              return [
+                PopupMenuItem(
+                  child: Text(
+                    'Only Favorites',
+                  ),
+                  value: FilterOption.Favorites,
+                ),
+                PopupMenuItem(
+                  child: Text(
+                    'Show All',
+                  ),
+                  value: FilterOption.All,
+                ),
+              ];
+            },
+            icon: Icon(
+              Icons.more_vert,
+            ),
+          )
+        ],
       ),
       body: ProductsGrid(),
     );
